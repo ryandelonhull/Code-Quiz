bdeb5e0e5b449389be3a9a08dbd95fdd358881ae
Ryan Hull <ryandelonhull@gmail.com>
2020-09-23 23:25:02 -0400

    add arrays and buttons.

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ var timerStart = document.querySelector("#start-button");
 // // //var container = document.querySelector(".container");
 // // var questions = document.querySelector("#questions");
 var questionNumber = -1;
-
+var yesButton = document.getElementById("yes-button")
+var noButton = document.getElementById("no-button")
+var maybeSoButton = document.getElementById("maybeSo-button")
+var finalScore = 0
 
 function toggleButtons(show){
    if (show){
@@ -27,38 +30,63 @@ var questionsArray = [
         title: "Where's the beef?",
         option1: "Yes",
         option2: "No",
+        option3: "maybeSo",
         answerCorrect: "maybeSo",
     },
     {
         title: "Does pineapple go on pizza?",
+        option1: "Yes",
+        option2: "No",
+        option3: "maybeSo",
         answerCorrect: "maybeSo",
     },
     {
         title: "Is cereal soup?",
+        option1: "Yes",
+        option2: "No",
+        option3: "maybeSo",
         answerCorrect: "maybeSo",
     },
     {
         title: "Is a hotdog a sandwich?",
+        option1: "Yes",
+        option2: "No",
+        option3: "maybeSo",
         answerCorrect: "maybeSo",
     },
     {
         title: "Does Bigfoot Exist?",
+        option1: "Yes",
+        option2: "No",
+        option3: "maybeSo",
         answerCorrect: "maybeSo",
     },
     {
         title: "Were dinosaurs just giant chickens and pigeons?",
+        option1: "Yes",
+        option2: "No",
+        option3: "maybeSo",
         answerCorrect: "maybeSo",
     },
     {
         title: "Are plants actually farming us for CO2 and extra compost?",
+        option1: "Yes",
+        option2: "No",
+        option3: "maybeSo",
         answerCorrect: "maybeSo",
     },
     {
         title: "Is truth even close to the same thing as reality?",
+        option1: "Yes",
+        option2: "No",
+        option3: "maybeSo",
         answerCorrect: "maybeSo",
     },
     {
         title: "Did your pet make up a name for you like you did for your pet?",
+        option1: "Yes",
+        option2: "No",
+        option3: "maybeSo",
         answerCorrect: "maybeSo",
     },
 ]
@@ -75,7 +103,8 @@ function startTimer() {
     //   }
       if(secondsLeft === 0) {
         clearInterval(timerInterval);
-        sendMessage();
+        questionNumber = questionsArray.length;
+        nextQuestion();
       }
   
     }, 1000);
@@ -97,8 +126,14 @@ function startTimer() {
   
 // }
 
-function nextQuestion (){
+function nextQuestion(){
     questionNumber++;
+    if (questionNumber >= questionsArray.length){
+        alert("Score is" + finalScore);
+        toggleButtons(false);
+        secondsLeft == 1;
+        return;
+    }
     var questionCurrent = questionsArray[questionNumber];
     var x = document.getElementById("question");
     x.innerHTML = questionCurrent.title;
@@ -106,11 +141,32 @@ function nextQuestion (){
 }
 function startGame (){
     startTimer();
-    nextQuestion();
-    
+    toggleButtons(true);
+    nextQuestion();    
 }
+function yesScore (){
+    if (questionsArray[questionNumber].answerCorrect == questionsArray[questionNumber].option1){
+        finalScore += 1;
+    }
+    nextQuestion()
+} 
+function noScore (){
+    if (questionsArray[questionNumber].answerCorrect == questionsArray[questionNumber].option2){
+        finalScore += 1;
+    }
+    nextQuestion()
+} 
+function maybeSoScore (){
+    if (questionsArray[questionNumber].answerCorrect == questionsArray[questionNumber].option3){
+        finalScore += 1;
+    }
+    nextQuestion()
+} 
 
 timerStart.addEventListener("click", startGame);
+yesButton.addEventListener("click", yesScore);
+noButton.addEventListener("click", noScore);
+maybeSoButton.addEventListener("click", maybeSoScore);
 
 // document.getElementById("start-button").addEventListener("click", function(){
 //     var secondsLeft = 60;
